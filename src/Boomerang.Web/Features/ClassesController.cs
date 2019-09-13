890079ac55e903306074e520af17a26aaef96d95
David Osborne <davidmosborne@yahoo.com>
2019-09-13 12:48:52 +0100

    Introduce Web and API projects;

---
+++ src/Boomerang.Web/Features/ClassesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Boomerang.Web.Features
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClassesController : ControllerBase
+    {
+        private readonly ICollection<ClassModel> _repository;
+
+        public ClassesController(ICollection<ClassModel> repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: Classes
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _repository.Select(c => c.Description);
+        }
+
+        // POST: Classes
+        [HttpPost]
+        public IActionResult Post([FromBody] ClassModel @class)
+        {
+            _repository.Add(@class);
+
+            return Ok();
+        }
+    }
+
+    public class ClassModel
+    {
+        public string Description { get; set; }
+
+        public DateTime StartsAt { get; set; }
+    }
+}
---
+++ test/Boomerang.Web.Tests.Integration/ClassesTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Boomerang.Web.Tests.Integration
+{
+    public class ClassesTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        public ClassesTests(WebApplicationFactory<Startup> webApplicationFactory)
+        {
+            _webApplicationFactory =
+                webApplicationFactory ?? throw new ArgumentNullException(nameof(webApplicationFactory));
+        }
+
+        private readonly WebApplicationFactory<Startup> _webApplicationFactory;
+
+        [Fact]
+        public async Task Get_ExistingClass_ShouldReturnClass()
+        {
+            // Arrange
+            var client = _webApplicationFactory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = null;
+
+            try
+            {
+                response =
+                    await client.PostAsJsonAsync(
+                        "classes",
+                        new
+                        {
+                            Description = "My Saturday Class",
+                            StartsAt = DateTime.UtcNow.ToString("O")
+                        });
+
+                response.EnsureSuccessStatusCode();
+
+                // Act
+                response = await client.GetAsync("classes");
+
+                // Assert
+                response.EnsureSuccessStatusCode(); // Status Code 200-299
+                var values = JsonConvert.DeserializeObject<string[]>(await response.Content.ReadAsStringAsync());
+
+                values.First().Should().Be("My Saturday Class");
+            }
+            finally
+            {
+                response?.Dispose();
+            }
+            
+        }
+    }
+}
