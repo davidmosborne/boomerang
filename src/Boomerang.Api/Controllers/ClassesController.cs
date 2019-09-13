47c84b71f759ac3f88a9f5f1f4754bfd32c479bb
David Osborne <davidmosborne@yahoo.com>
2019-09-13 13:25:25 +0100

    Foundation with tests;

---
+++ src/Boomerang.Api/Controllers/ClassesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Boomerang.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClassesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[]
+            {
+                "value1",
+                "value2"
+            };
+        }
+    }
+}
---
+++ test/Boomerange.Api.Tests.Integration/ClassesTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Boomerang.Api.Tests.Integration
+{
+    public class ClassesTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        public ClassesTests(WebApplicationFactory<Startup> webApplicationFactory)
+        {
+            _webApplicationFactory =
+                webApplicationFactory ?? throw new ArgumentNullException(nameof(webApplicationFactory));
+        }
+
+        private readonly WebApplicationFactory<Startup> _webApplicationFactory;
+
+        [Fact]
+        public async Task Get_ExistingClass_ShouldReturnClass()
+        {
+            // Arrange
+            var client = _webApplicationFactory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = null;
+
+            try
+            {
+                // Act
+                response = await client.GetAsync("classes");
+
+                // Assert
+                response.EnsureSuccessStatusCode(); // Status Code 200-299
+                
+            }
+            finally
+            {
+                response?.Dispose();
+            }
+            
+        }
+    }
+}
